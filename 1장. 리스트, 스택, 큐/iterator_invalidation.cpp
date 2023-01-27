ccd3d0931b53d37cd377774b2615041735d3cc49
Kyungbin Cho <diesuki4@gmail.com>
2023-01-27 21:32:52 +0900

    Create iterator_invalidation.cpp

---
+++ 1장. 리스트, 스택, 큐/iterator_invalidation.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+void main()
+{
+    vector<int> v = {1, 2, 3, 4, 5};
+
+    // 용량을 10으로 변경
+    v.reserve(10);
+
+    // it는 5의 위치를 가리킨다.
+    vector<int>::iterator v_it = v.begin() + 4;
+
+    cout << *v_it << endl;
+
+    // 3번째 위치에 0 추가
+    v.emplace(v.begin() + 2, 0);
+
+    // 오류 발생!!
+    // cout << *v_it << endl;
+
+    // 오류가 발생하지 않는다.
+    cout << *(v.begin() + 4) << endl;
+
+
+
+    list<int> lst = {1, 2, 3, 4, 5};
+
+    list<int>::iterator lst_it = next(lst.begin(), 4);
+
+    cout << *lst_it << endl;
+
+    // 3번째 위치에 0 추가
+    lst.emplace(prev(lst_it, 1), 0);
+
+    // 오류가 발생하지 않는다.
+    cout << *lst_it << endl;
+}
