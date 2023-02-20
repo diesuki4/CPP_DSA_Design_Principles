d14b16bf55c2bfd00ce20cad9b1a147ad5c6da87
Kyungbin Cho <diesuki4@gmail.com>
2023-02-20 22:29:22 +0900

    Create std_partition_point.cpp

---
+++ 4장. 분할 정복/std_partition_point.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+void print(vector<int> v);
+
+void main()
+{
+    vector<int> v{9, 2, 1, 6, 5, 4, 7, 8, 3};
+    auto is_odd = [](int e) { return e & 1; };
+
+    partition(v.begin(), v.end(), is_odd);
+
+    print(v);
+
+    cout << "파티션에서 홀수가 아닌 첫 위치: " << distance(v.begin(), partition_point(v.begin(), v.end(), is_odd)) << endl;
+}
