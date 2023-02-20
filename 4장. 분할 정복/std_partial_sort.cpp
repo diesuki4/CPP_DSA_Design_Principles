e5d83783285629b52c08de7ebedea08f77870e77
Kyungbin Cho <diesuki4@gmail.com>
2023-02-20 22:29:34 +0900

    Create std_partial_sort.cpp

---
+++ 4장. 분할 정복/std_partial_sort.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+void print(vector<int> v);
+
+void main()
+{
+    vector<int> v{9, 2, 1, 6, 5, 4, 7, 8, 3};
+
+    partial_sort(v.begin() + 2, v.begin() + 5, v.end());
+    print(v);
+}
